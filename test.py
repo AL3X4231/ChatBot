d6cdf224a3182795dea84b1908fad21dd93228e9
AL3X4231 <alex2005morin@gmail.com>
2023-11-26 18:36:26 +0100

    fonctionnalité 3

--- test.py
+++ test.py
@@ -87,4 +87,26 @@ def matrix():
 
     return matrice
 
-print(matrix())
+
+def fonctionnalité3():
+    with open("cleaned/Nomination_Chirac1.txt","r",encoding='utf8') as document1:
+        sentence1=document1.read()
+    with open("cleaned/Nomination_Chirac2.txt","r",encoding='utf8') as document2:
+        sentence2=document2.read()
+    sentence=Tf(sentence1+sentence2)
+    max=0
+    word=[]
+    for i,j in sentence.items():
+        if j>max:
+            max=j
+            word=[i]
+        elif j==max:
+            word.append(i)
+    return word
+
+
+
+
+
+
+print(fonctionnalité3())
