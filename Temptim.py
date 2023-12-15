fe7cb91f43b634c542583d785cb4115e2ffee25b
Timothee-Robin <151126893+Timothee-Robin@users.noreply.github.com>
2023-12-15 10:04:37 +0100

    Update Temptim.py

--- Temptim.py
+++ Temptim.py
@@ -14,6 +14,11 @@ matrix_e=matrix()
 
 intersection=intersection_qst_corpus(qst,matrix_e)
 
+dicti={'le':[0,1,0,1,0,2,3,9],'ruban':[0,1,0,1,0,2,3,9],'est':[0,1,0,1,0,2,3,9],'bleu':[0,1,0,1,0,2,3,9]}
+
+def idf_qst():
+    
+    
 print(intersection)
 
 
